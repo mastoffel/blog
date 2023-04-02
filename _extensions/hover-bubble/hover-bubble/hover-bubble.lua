6ebcefc6a6ff02b036a059208fefbbef4997886a
mastoffel <martin.adam.stoffel@gmail.com>
2023-04-02 20:37:32 +0100

    Add hover-bubble filter

---
+++ _extensions/hover-bubble/hover-bubble/hover-bubble.lua
@@ -0,0 +1,15 @@
+
+-- Create hover bubble
+function Span(el)
+  if el.classes:includes("hover-bubble") then
+      local hover_text = el.attributes['hover-text']
+      if hover_text then
+          local span = pandoc.Span(hover_text, {class = "hover-text"})
+          el.content:insert(span)
+          return el
+      end
+  end
+end
+
+
+
